94595b21aabe97184ee83bb92f8e48e4977ebb28
Dhruv Srivastava <dhruvdhs.ds@gmail.com>
2022-04-27 18:54:12 +0530

    pause

--- test.go
+++ test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"math"
 )
@@ -28,34 +29,35 @@ func (n *node) getNodeHash() {
 
 		out := sha256.Sum256([]byte(n.val))
 		n.nodeHash = out[:]
+		fmt.Println(hex.EncodeToString(n.nodeHash) + " for " + n.val)
 	} else {
 
 		// if node contains only 1 child
 		// transfer the same hash upwards
 
-		if n.right == nil {
-
+		if n.right == nil && n.left != nil {
 			n.nodeHash = n.left.nodeHash
+		} else {
+
+			// else node contains 2 children
+			// parent hash =  hash (child1 | child2)
+			hashSlice := []byte{}
+			hashSlice = append(hashSlice, n.left.nodeHash...)
+			hashSlice = append(hashSlice, n.right.nodeHash...)
+			fmt.Println(hex.EncodeToString(hashSlice))
+
+			temp := sha256.Sum256(hashSlice)
+			n.nodeHash = temp[:]
+			fmt.Println(hex.EncodeToString(n.nodeHash))
 		}
-
-		// else node contains 2 children
-		// parent hash =  hash (child1 | child2)
-		hashSlice := []byte{}
-		hashSlice = append(hashSlice, n.left.nodeHash...)
-		hashSlice = append(hashSlice, n.right.nodeHash...)
-
-		temp := sha256.Sum256(hashSlice)
-		n.nodeHash = temp[:]
-
 	}
 }
 
+// takes data array input and returns a merkle tree
 func buildTree(arr []string) *merkleTree {
 
 	n := len(arr)
-
 	// height of tree = ceil (log(n))
-	// max nodes in tree of height h =
 
 	outTree := new(merkleTree)
 	outTree.n = n
@@ -73,13 +75,32 @@ func buildTree(arr []string) *merkleTree {
 		outTree.leafNodes[i].getNodeHash()
 	}
 
-	//
 	// iterate level wise
 	tempSlice := outTree.leafNodes
 
-	for level := math.Ceil(math.Log2(float64(n))) - 1; level > 0; level-- {
+	for level := math.Ceil(math.Log2(float64(n))) - 1; level >= 0; level-- {
 
 		for nodex := 0; nodex <= int(math.Pow(2, level-1)); nodex += 2 {
+
+			if tempSlice[nodex].nodeHash == nil {
+				break
+			}
+			if !tempSlice[nodex].isLeaf && tempSlice[nodex].right == nil {
+				tempParent := &node{
+					left:   &tempSlice[nodex],
+					right:  nil,
+					isLeaf: false,
+				}
+				tempSlice[nodex].parent = tempParent
+				tempParent.getNodeHash()
+				tempSlice[nodex/2] = *tempParent
+
+				if level == 1 {
+					outTree.root = tempParent
+					outTree.rootHash = tempParent.nodeHash
+				}
+				break
+			}
 
 			nodey := nodex + 1
 
@@ -101,8 +122,8 @@ func buildTree(arr []string) *merkleTree {
 			}
 		}
 	}
-	fmt.Println(outTree.root)
-	fmt.Println((outTree.rootHash))
+
+	// fmt.Println((outTree.rootHash))
 	return outTree
 }
 
@@ -114,19 +135,23 @@ func main() {
 
 	words := []string{"hello", "this", "is", "a", "merkle", "tree"}
 
-	bruh := buildTree(words)
+	Mtree := buildTree(words)
 
-	fmt.Println((bruh.rootHash))
-	fmt.Println(bruh.root)
-	// fmt.Println(bruh.root.right)
-	// fmt.Println(bruh.root.left)
+	fmt.Println(hex.EncodeToString(Mtree.rootHash))
 
-	tempSlices := []byte{}
-	for _, x := range words {
-		tempSlices = append(tempSlices, []byte(x)...)
-	}
+	// nodeHashes of left and right nodes
+	fmt.Println("left hash is ")
+	fmt.Println(hex.EncodeToString(Mtree.root.left.nodeHash))
 
-	tempHash := sha256.Sum256(tempSlices)
-	fmt.Println(tempHash)
+	fmt.Println("right hash is")
+	fmt.Println(hex.EncodeToString(Mtree.root.right.nodeHash))
 
+	// calculate hash of left and right nodes after appending
+	temp := []byte{}
+	temp = append(temp, Mtree.root.left.nodeHash...)
+	temp = append(temp, Mtree.root.right.nodeHash...)
+	// fmt.Println(hex.EncodeToString(temp))
+	temp2 := sha256.Sum256(temp)
+	// final (supposed to be) merkle hash
+	fmt.Println(hex.EncodeToString(temp2[:]))
 }
